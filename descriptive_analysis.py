f18d0cdc56fac27c4a8c01eaf8fff585a1f3e476
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-11-17 01:02:08 +0200

    Descriptive analysis: Implemented simple descriptive analysis.

--- descriptive_analysis.py
+++ descriptive_analysis.py
@@ -10,6 +10,52 @@ def run(input_path):
     # Read the data set
     data = read_data(input_path)
 
+    print('--------------------------------------------------')
+    print('Descriptive analysis')
+    print('--------------------------------------------------')
+
+    # Analyse keys
+    keys = sorted(data.keys())
+    print('There are ' + str(len(keys)) + ' keys in total:')
+    print('  ' + str(keys))
+    print('')
+
+    # Analyse subjects
+    subjects = sorted(list(set(data['subjects'])))
+    print('There are ' + str(len(subjects)) + ' unique subjects tests were ' +
+          'performed on:')
+    print('  ' + str(subjects))
+    print('Their frequencies within the data set are the following:')
+    # TODO
+    print('')
+
+    # Analyse Brodmann areas
+    areas = sorted(list(set(data['areas'])))
+    print('There are ' + str(len(areas)) + ' unique Brodmann areas used in ' +
+          'the tests:')
+    print('  ' + str(areas))
+    print('Their frequencies within the data set are the following:')
+    # TODO
+    print('')
+
+    # Analyse image categories
+    image_categories = sorted(list(set(data['image_category'])))
+    print('There are ' + str(len(image_categories)) + ' unique image ' +
+          'categories that the images have been classified into:')
+    print('  ' + str(image_categories))
+    print('Their frequencies within the data set are the following:')
+    # TODO
+    print('')
+
+    # Analyse tests
+    print('In total, there were ' + str(len(data['subjects'])) + ' tests ' +
+          'performed on each of the ' + str(len(data['image_category'])) + ' ' +
+          'images. For each of these test and image pairs, there is an ' +
+          'integer denoting the neural response in the specifed Brodmann ' +
+          'area of the specified patient after showing them the specified ' +
+          'image.')
+
+
     # TODO: Analysis
 
 
