c5173e1431a975e3f82fe77f3efdc1731c94be88
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-11-29 09:15:28 +0200

    SVM: Fixed formatting.

--- svm.py
+++ svm.py
@@ -5,28 +5,31 @@ from argparse import ArgumentParser
 from lib.io import read_data
 from sklearn import svm
 
+
 def run(train_path, test_path):
-	 train_data = read_data(train_path)
-	 test_data = read_data(test_path)
-	
-	 print('Performing SVM classifier...')
-	 svcClassif = svm.SVC()
-	 print('Fitting training samples to create a training model...')
-	 
-	 #TODO not sure if this is the correct type of data that should be used for fitting, but this
-	 #should be the general sequence of the classification
-	 
-	 #first parameter: array of size [n_samples, n_features], second parameter: array of size [n_samples]
-	 svcClassif.fit(train_data['neural_responses'], train_data['image_category'])
-	 print('Performing classification on test samples...')
-	 results=svcClassif.predict(test_data['neural_responses'])
-	 print 'Resulting classifications: ', results
-	 print 'Actual classes: ', test_data['image_category']
-	
+    
+    # Read input data
+    train_data = read_data(train_path)
+    test_data = read_data(test_path)
+
+    print('Performing SVM classifier...')
+    svcClassif = svm.SVC()
+    print('Fitting training samples to create a training model...')
+
+    #TODO not sure if this is the correct type of data that should be used for fitting, but this
+    #should be the general sequence of the classification
+
+    #first parameter: array of size [n_samples, n_features], second parameter: array of size [n_samples]
+    svcClassif.fit(train_data['neural_responses'], train_data['image_category'])
+    print('Performing classification on test samples...')
+    results=svcClassif.predict(test_data['neural_responses'])
+    print 'Resulting classifications: ', results
+    print 'Actual classes: ', test_data['image_category']
+
 
 if __name__ == '__main__':
 
-# Parse command line arguments
+    # Parse command line arguments
     PARSER = ArgumentParser()
     PARSER.add_argument('train_path', help='the pickled output training data \
                                             file path')
