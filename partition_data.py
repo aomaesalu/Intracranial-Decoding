8f7ec6ea3ee43ca740bb5e0729fa4e5345fd7878
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-11-29 13:08:21 +0200

    Partitioning: Implemented even distributions.

--- partition_data.py
+++ partition_data.py
@@ -9,16 +9,31 @@ from random import sample
 from lib.io import read_data, write_data
 
 
-def run(input_path, train_path, test_path, train_size):
+def run(input_path, train_path, test_path, train_size, is_even):
 
     # Read the data set
     data = read_data(input_path)
 
-    # Generate the training set indices
+    # Find the number of images in the data set
     number_of_images = len(data['image_category'])
-    train_indices = sorted(sample(range(number_of_images),
-                                  int(number_of_images * float(train_size) \
-                                      / 100)))
+
+    # Find all image classes in the data set
+    classes = sorted(set(data['image_category']))
+
+    # Construct a list of indices of appropriate images for each image class
+    index_lists = {}
+    for image_class in classes:
+        index_lists[image_class] = []
+    for i in range(number_of_images):
+        index_lists[data['image_category'][i]].append(i)
+
+    # Generate the training set indices
+    train_indices = []
+    for image_class in classes:
+        images_in_class = len(index_lists[image_class])
+        train_indices += sample(index_lists[image_class], \
+                                int(images_in_class * float(train_size) / 100))
+    train_indices.sort()
 
     # Calculate the test set indices
     test_indices = sorted(list(set(range(number_of_images)) \
@@ -47,16 +62,17 @@ if __name__ == '__main__':
 
     # Parse command line arguments
     PARSER = ArgumentParser()
-    PARSER.add_argument('input_path', help='the pickled input data file \
-                                            containing the neural responses \
-                                            data')
-    PARSER.add_argument('train_path', help='the pickled output training data \
-                                            file path')
-    PARSER.add_argument('test_path', help='the pickled output testing data \
-                                           file path')
+    PARSER.add_argument('input_path', help='the pickled input data file ' +
+                        'containing the neural responses data')
+    PARSER.add_argument('train_path', help='the pickled output training data ' +
+                        'file path')
+    PARSER.add_argument('test_path', help='the pickled output testing data ' +
+                        'file path')
     PARSER.add_argument('train_size', help='the size of the training data set')
+    PARSER.add_argument('--even', help='ensure that the distribution of ' +
+                        'classes in partitions is even', action='store_true')
     ARGUMENTS = PARSER.parse_args()
 
     # Run the data partition script
     run(ARGUMENTS.input_path, ARGUMENTS.train_path, ARGUMENTS.test_path,
-        ARGUMENTS.train_size)
+        ARGUMENTS.train_size, ARGUMENTS.even)
