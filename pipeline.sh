814e8f0a5b9f1877576ac97905f143695cc83159
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-11-30 06:15:45 +0200

    Cross-validation: Refactored pipeline to correspond with the new stratified k-fold cross-validation methodology.

--- pipeline.sh
+++ pipeline.sh
@@ -3,7 +3,9 @@
 # Descriptive analysis of the data set. This is not necessaryly a part of the
 # pipeline, as the data set does not change, but it is still currently included
 # for the purposes of clarity.
+printf '# Pipeline: Descriptive analysis\n'
 python2.7 ./descriptive_analysis.py data/stim_probe_category_ON_meangamma_bipolar_noscram_artif_brodmann_resppositive.pkl
+printf '\n'
 
 # Set into how many equal parts to partition the data.
 k=10
@@ -14,17 +16,11 @@ python2.7 ./partition_data.py data/stim_probe_category_ON_meangamma_bipolar_nosc
 printf '\n'
 
 # Analysis: SVM
-# for (( i=1; i<=N; i++ ))
-# do
-#     printf '# Pipeline: SVM (%d)\n' $i
-#     python2.7 ./svm.py data/train-$i.pkl data/test-$i.pkl
-#     printf '\n'
-# done
-#
-# # Analysis: Random forests
-# for (( i=1; i<=N; i++ ))
-# do
-#     printf '# Pipeline: Random forests (%d)\n' $i
-#     python2.7 ./random_forest.py data/train-$i.pkl data/test-$i.pkl
-#     printf '\n'
-# done
+printf '# Pipeline: SVM\n'
+python2.7 ./svm.py data/partition.pkl $k
+printf '\n'
+
+# Analysis: SVM
+printf '# Pipeline: Random forests\n'
+python2.7 ./random_forest.py data/partition.pkl $k
+printf '\n'
