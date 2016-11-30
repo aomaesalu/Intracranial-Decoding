7f7e213249e018dc86ee76d80f5b38c05353708e
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-11-30 06:28:58 +0200

    Cross-validation: Implemented training and test set construction for k-fold cross-validation in the random forests algorithm script.

--- random_forest.py
+++ random_forest.py
@@ -15,18 +15,37 @@ def run(data_path, cv_amount):
     for i in range(cv_amount):
         data.append(read_data(format_path(data_path, i + 1)))
 
-    # # Classification
-    # model = RandomForestClassifier(n_estimators=2000)#, max_features=10, max_leaf_nodes=15, max_depth=5)
-    # model.fit(train_data['neural_responses'], train_data['image_category'])
-    #
-    # # Prediction
-    # prediction = model.predict(test_data['neural_responses'])
-    #
-    # # Scoring
-    # confusion_matrix = ConfusionMatrix(test_data['image_category'], prediction)
-    # print(confusion_matrix)
-    # score = Score(test_data['image_category'], prediction)
-    # print(score)
+    # Iterate through all of the data sets, using each of them for test data
+    # exactly once, and using all others as training data sets at the same
+    for test_index in range(cv_amount):
+
+        # Construct training and test data sets
+        train_data = {
+            'subjects': data[0]['subjects'],
+            'areas': data[0]['areas'],
+            'image_category': [],
+            'neural_responses': []
+        }
+        for i in range(cv_amount):
+            if i == test_index:
+                continue
+            for field in ['image_category', 'neural_responses']:
+                train_data[field] += data[i][field]
+        test_data = data[test_index]
+
+        # Classification
+        model = RandomForestClassifier(n_estimators=500)#, max_features=10, max_leaf_nodes=15, max_depth=5)
+        model.fit(train_data['neural_responses'], train_data['image_category'])
+
+        # Prediction
+        prediction = model.predict(test_data['neural_responses'])
+
+        # Scoring
+        confusion_matrix = ConfusionMatrix(test_data['image_category'],
+                                           prediction)
+        print(confusion_matrix)
+        score = Score(test_data['image_category'], prediction)
+        print(score)
 
 if __name__ == '__main__':
 
