d38e939c935e51232beccf7b229cd7f2860e769e
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-11-30 08:08:27 +0200

    Pipeline: Parameterised file paths.

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Define file paths
+data_file='data/stim_probe_category_ON_meangamma_bipolar_noscram_artif_brodmann_resppositive.pkl'
+partition_file='data/partition.pkl'
+
 # Descriptive analysis of the data set. This is not necessaryly a part of the
 # pipeline, as the data set does not change, but it is still currently included
 # for the purposes of clarity.
 printf '# Pipeline: Descriptive analysis\n'
-python2.7 ./descriptive_analysis.py data/stim_probe_category_ON_meangamma_bipolar_noscram_artif_brodmann_resppositive.pkl
+python2.7 ./descriptive_analysis.py $data_file
 printf '\n'
 
 # Set into how many equal parts to partition the data.
@@ -12,15 +16,15 @@ k=10
 
 # Data partitioning. We use stratified k-fold cross-validation.
 printf '# Pipeline: Partitioning data\n'
-python2.7 ./partition_data.py data/stim_probe_category_ON_meangamma_bipolar_noscram_artif_brodmann_resppositive.pkl data/partition.pkl $k --even
+python2.7 ./partition_data.py $data_file $partition_file $k --even
 printf '\n'
 
 # Analysis: SVM
 printf '# Pipeline: SVM\n'
-python2.7 ./svm.py data/partition.pkl $k
+python2.7 ./svm.py $partition_file $k
 printf '\n'
 
 # Analysis: SVM
 printf '# Pipeline: Random forests\n'
-python2.7 ./random_forest.py data/partition.pkl $k
+python2.7 ./random_forest.py $partition_file $k
 printf '\n'
