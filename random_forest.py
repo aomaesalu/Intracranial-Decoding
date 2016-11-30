7538638b20136a47862465e583c72979522b7cd2
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-11-30 06:12:05 +0200

    General: Removed unnecessary code.

--- random_forest.py
+++ random_forest.py
@@ -7,9 +7,6 @@ from lib.string import format_path
 from lib.score import ConfusionMatrix, Score
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 
-from collections import Counter
-import numpy as np
-
 
 def run(data_path, cv_amount):
 
@@ -17,65 +14,6 @@ def run(data_path, cv_amount):
     data = []
     for i in range(cv_amount):
         data.append(read_data(format_path(data_path, i + 1)))
-
-    # for i in range(len(train_data['image_category'])):
-    #     if train_data['image_category'][i] != 80:
-    #         train_data['image_category'][i] = 0
-    # for i in range(len(test_data['image_category'])):
-    #     if test_data['image_category'][i] != 80:
-    #         test_data['image_category'][i] = 0
-    #
-    # faces = []
-    # for i in range(len(train_data['image_category'])):
-    #     if train_data['image_category'][i] == 80:
-    #         faces.append(i)
-    #
-    # while len(train_data['image_category']) <= 5000:
-    #     for i in faces:
-    #         train_data['image_category'].append(train_data['image_category'][i])
-    #         train_data['neural_responses'].append(train_data['neural_responses'][i])
-    #         if len(train_data['image_category']) > 5000:
-    #             break
-
-    # counter = Counter(train_data['areas'])
-    # removed_areas = set()
-    # for key, value in counter.items():
-    #     if value < 10:
-    #         removed_areas.add(key)
-    # print('Removed areas:')
-    # print(removed_areas)
-    #
-    # removed_indices = []
-    # for i in range(len(train_data['areas'])):
-    #     if train_data['areas'][i] in removed_areas:
-    #         removed_indices.append(i)
-    # print('Removed indices')
-    # print(removed_indices)
-    #
-    # train_data['neural_responses'] = np.delete(train_data['neural_responses'], removed_indices, 1)
-    # test_data['neural_responses'] = np.delete(test_data['neural_responses'], removed_indices, 1)
-    #
-    # print(len(train_data['neural_responses'][0]))
-    #
-    # counter = Counter(train_data['subjects'])
-    # removed_subjects = set()
-    # for key, value in counter.items():
-    #     if value < 5:
-    #         removed_subjects.add(key)
-    # print('Removed subjects:')
-    # print(removed_subjects)
-    #
-    # removed_indices = []
-    # for i in range(len(train_data['subjects'])):
-    #     if train_data['subjects'][i] in removed_subjects:
-    #         removed_indices.append(i)
-    # print('Removed indices')
-    # print(removed_indices)
-    #
-    # train_data['neural_responses'] = np.delete(train_data['neural_responses'], removed_indices, 1)
-    # test_data['neural_responses'] = np.delete(test_data['neural_responses'], removed_indices, 1)
-    #
-    # print(len(train_data['neural_responses'][0]))
 
     # # Classification
     # model = RandomForestClassifier(n_estimators=2000)#, max_features=10, max_leaf_nodes=15, max_depth=5)
