8e962c7b9e59e660a856a26d06c307b56c806af4
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-12-01 12:58:50 +0200

    Scoring: Initialised result class.

--- lib/classification.py
+++ lib/classification.py
@@ -4,14 +4,60 @@
 from .io import read_data
 from .string import add_suffix_to_path
 from .cross_validation import construct_data_sets
-from .score import ConfusionMatrix, Score
+#from .score import ConfusionMatrix, Score
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
+
+
+class Result(object):
+
+    def __init__(self):#, labels):
+
+        # # Set the label list from the parameter passed to the constructor
+        # self.labels = labels
+
+        # Intialise the true and predicted value lists
+        self.true_values = []
+        self.predicted_values = []
+
+        # Initialise the result confusion matrix and scores per each class
+        self.confusion_matrix = None
+        self.scores_per_class = None
+        self.average_scores = None
+
+
+    def add_values(self, true_values, predictions):
+
+        # Append the new true and predicted values to the existing lists
+        self.true_values.append(true_values)
+        self.predictions.append(predictions)
+
+
+    def calculate(self):
+
+        # Calculate the score matrix
+        self.confusion_matrix = confusion_matrix(self.true_values,
+                                                 self.predicted_values)#,
+                                                 #self.labels)
+
+        # Calculate the scores per each class
+        pass # TODO
+
+        # Calculate the average score
+        # We are using macro averaging because it doesn't take class
+        # distribution inbalance into account. Each class is as important as
+        # another.
+        score_labels = ['precision', 'recall', 'f1', 'support']
+        average_scores = precision_recall_fscore_support(self.true_values,
+                                                         self.predicted_values,
+                                                         average='macro')
+        for i in range(len(score_labels)):
+            self.average_scores[score_labels[i]] = average_scores[i]
 
 
 def classify(data_path, partitions, iterations, model):
 
-    # Initialise the true value and prediction lists
-    true_values = []
-    predictions = []
+    # Initialise the result of the classification
+    result = Result()
 
     # Repeat cross-validation a set amount of times
     for iteration in range(iterations):
