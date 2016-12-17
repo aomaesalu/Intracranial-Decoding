650fb61cfe1ae32bb564fca8e3455e6c8aba3a6d
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-12-17 15:56:03 +0200

    Grid search: Implemented parameter classes and random sampling.

--- lib/cross_validation.py
+++ lib/cross_validation.py
@@ -1,6 +1,39 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf8 -*-i
 
+from random import choice, randint, uniform
+
+
+class DistinctParameter(object):
+
+    def __init__(self, values):
+        self.values = values
+
+    def generate(self):
+        return choice(self.values)
+
+
+class ContinuousParameter(object):
+
+    def __init__(self, low, high, sampler):
+        self.low = low
+        self.high = high
+
+    def generate(self):
+        return uniform(self.low, self.high)
+
+
+class IntParameter(ContinuousParameter):
+
+    def __init(self, low, high):
+        super(IntParameter, self).__init__(low, high, randint)
+
+
+class FloatParameter(ContinuousParameter):
+
+    def __init__(self, low, high):
+        super(FloatParameter, self).__init__(low, high, uniform)
+
 
 def construct_data_sets(data, cv_amount, test_index):
 
