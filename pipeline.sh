a944461f6a0612332401e00525c50fb44db46e50
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2016-12-22 02:48:23 +0200

    Pipeline: Saved time windows and frequency bands into strings.

--- pipeline.sh
+++ pipeline.sh
@@ -12,6 +12,10 @@ raw_partitioned_data_file='data/partitioned/TIMEWINDOW_FREQUENCYBAND.pkl'
 raw_grid_search_file='data/grid_search/TIMEWINDOW_FREQUENCYBAND.pkl'
 raw_ensemble_file='data/ensemble/TIMEWINDOW_FREQUENCYBAND.pkl'
 
+# Initialise the values of time windows and frequency bands researched
+time_windows="50 150 250"
+frequency_bands="theta alpha beta lowgamma highgamma"
+
 # Set cross-validation parameters
 partitions=10
 iterations=10
@@ -24,10 +28,12 @@ best_proportion=0.1
 used_proportion=0.1
 
 # Iterate through each possible time window
-for time_window in 50 150 250; do
+for time_window in $time_windows
+do
 
   # Iterate through each possible frequency band
-  for frequency_band in theta alpha beta lowgamma highgamma; do
+  for frequency_band in $frequency_bands
+  do
 
     # Construct file names specific to the current time window and freqeuncy
     # band
@@ -42,19 +48,19 @@ for time_window in 50 150 250; do
     partitioned_data_file_extension=${partitioned_data_file##*.}
 
     # Filter the raw data by classes
-    printf '# Pipeline: Filtering data by classes'
+    printf '# Pipeline ($d, $s): Filtering data by classes' $time_window $frequency_band
     python ./filter_by_class.py $data_file $filtered_data_file 10 20 30 40 50
     printf '\n\n'
 
     # Descriptive analysis of the data set. This is not necessaryly a part of
     # the pipeline, as the data set does not change, but it is still currently
     # included for the purposes of clarity.
-    printf '# Pipeline: Descriptive analysis\n'
+    printf '# Pipeline ($d, $s): Descriptive analysis\n' $time_window $frequency_band
     python ./descriptive_analysis.py $filtered_data_file
     printf '\n'
 
     # Data partitioning. We use stratified k-fold cross-validation N times.
-    printf '# Pipeline: Partitioning data %d times into %d equal sets\n' $iterations $partitions
+    printf '# Pipeline ($d, $s): Partitioning data %d times into %d equal sets\n' $time_window $frequency_band $iterations $partitions
     for (( i=1; i<=iterations; i++ ))
     do
         python ./partition_data.py $filtered_data_file $partitioned_data_file_name'-'$i'.'$partitioned_data_file_extension $partitions --even
@@ -62,12 +68,12 @@ for time_window in 50 150 250; do
     printf '\n'
 
     # Classification and grid search
-    printf '# Pipeline: Classification and grid search\n'
+    printf '# Pipeline ($d, $s): Classification and grid search\n' $time_window $frequency_band
     python ./classify.py $partitioned_data_file $grid_search_file $partitions $iterations $trials
     printf '\n'
 
     # Ensemble construction, classification and scoring
-    printf '# Pipeline: Ensemble construction, classification and scoring\n'
+    printf '# Pipeline ($d, $s): Ensemble construction, classification and scoring\n' $time_window $frequency_band
     python ./ensemble.py $partitioned_data_file $grid_search_file $ensemble_file $partitions $iterations $best_proportion $used_proportion
     printf '\n'
 
